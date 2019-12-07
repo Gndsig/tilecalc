38399c8641026d8714a88118ce190e287e72b8d4
Gndsig <hiyokomamitsu@gmail.com>
2019-12-07 13:44:49 +0000

    Once completed

--- example/debag/function_debag3.py
+++ example/debag/function_debag3.py
@@ -10,7 +10,7 @@ import glob
 #%%
 line_sample1 = 'LINESTRING(139.07730102539062 36.00022956359412, 139.0814208984375 35.98022880246021)'
 ls1 = shapely.wkt.loads(line_sample1)
-ls2 = LineString([(139.07455444335938, 35.9913409624497), (139.07730102539062, 35.99356320664446),(139.07867431640625,35.99722880246021),(139.0814208984375,35.99822880246021)])
+ls2 = LineString([(139.07485444335938, 35.9923409624497), (139.07730102539062, 35.99356320664446),(139.07867431640625,35.99722880246021),(139.0824208984375,35.99842880246021)])
 
 
 #%%
@@ -38,10 +38,11 @@ if TILE_SIZE=='self' and hasattr(self, 'TILE_SIZE'):
     TILE_SIZE = self.TILE_SIZE
 elif TILE_SIZE=='self' and not hasattr(self, 'TILE_SIZE'):
     raise KeyError('TILE_SIZE is not found. Please input TILE_SIZE or in class instance')
-        
-if not filepath[-1:]=='/':
-    filepath = filepath+'/'
-filepath = filepath + str(zoom) + '/'
+
+if not filepath:     
+    if not filepath[-1:]=='/':
+        filepath = filepath+'/'
+    filepath = filepath + str(zoom) + '/'
 
 if len(bounds)==4:  # if bounds=(x_min,ymin,x_max,y_max)
     bounds = [0, np.array([[bounds[0],bounds[1]],[bounds[2],bounds[1]],[bounds[2],bounds[3]],[bounds[0], bounds[3]]]) ]
@@ -75,66 +76,250 @@ for y in y_set:
     points = np.concatenate([points,xy], axis=0)
 len_points = lx*ly
 
-# linking tile and points
-# Tiles are represented by 4 points number
+# linking tiles and points
+# tiles are represented by 4 points number
 # 6---7---8 
 # | 2'| 3'|
 # 3---4---5
 # | 0'| 1'|       
 # 0---1---2
 # 0,1,2 : points number, 0',1',2' : tiles number
-# this case, tile 0' is [0, 1, 4, 3]
+# this case, tiles 0' is [0, 1, 4, 3]
+
 
 lx1 = lx-1
 ly1 = ly-1
-tile = []
-tile_append = tile.append
+tiles = []
+tiles_append = tiles.append
 for j in range(ly1):
     for i in range(lx1):
-        tile_append([i +j+lx1*j, i+1 +j+lx1*j, i+2+lx1 +j+lx1*j, i+1+lx1 +j+lx1*j])
-len_tile = len(tile)
-tile = np.array(tile)
+        tiles_append([i +j+lx1*j, i+1 +j+lx1*j, i+2+lx1 +j+lx1*j, i+1+lx1 +j+lx1*j])
+len_tiles = len(tiles)
+tiles = np.array(tiles)
+
+# linking tiles and lines, lines and points
+lines = []  # all line segments composed of points
+lines_append = lines.append
+owner = []  # Which tile the lines belong to
+owner_append = owner.append
+for j in range(ly):
+    for i in range(lx1):
+        lines_append([i +j+lx1*j, i+1 +j+lx1*j])
+        if j==0:
+            owner_append([i +lx1*j])
+        elif j==ly-1:
+            owner_append([i-lx1 + lx1*j])
+        else:
+            owner_append([i-lx1 + lx1*j, i +lx1*j])
+            
+for j in range(ly1):
+    for i in range(lx):
+        lines_append([i +j+lx1*j, i+1+lx1 +j+lx1*j])
+        if i==0:
+            owner_append([i +lx1*j])
+        elif i==lx-1:
+            owner_append([i-1 + lx1*j])
+        else:
+            owner_append([i-1 + lx1*j, i +lx1*j])
+
+len_lines = len(lines)
+lines = np.array(lines)
+
 
 # judgement points are whether in bounds.
 points_is_in_bounds = np.array(list(map(functools.partial(self.inpolygon, polygon=pixel_bounds), points)))
 
-# tile have how many points in bounds.
+
+
+# intersects with bounds when the end point of the line is True and False
+line_points_in_bounds = []
+line_points_in_bounds_append = line_points_in_bounds.append
+for i in range(len_lines):
+    howmany_in_bounds = points_is_in_bounds[lines[i]].sum()
+    line_points_in_bounds_append(howmany_in_bounds)
+
+# lines having cross==True points and False points, line is cross to bounds.
+cross_lines = [lpib==1 for lpib in line_points_in_bounds]
+
+
+
+# Judgment which bounds the intersecting line intersects
+cross_lines_coords = points[lines[cross_lines]]
+len_cross_lines_coords = len(cross_lines_coords)
+
+# pixel_bounds having 4 lines, its represent points.
+len_pixel_bounds = len(pixel_bounds)
+pb_lines_coords = []
+pb_lines_coords_append = pb_lines_coords.append
+for i in range(len_pixel_bounds):
+    if i!=len_pixel_bounds-1:
+        pb_lines_coords_append([pixel_bounds[i],pixel_bounds[i+1]])
+    elif i==len_pixel_bounds-1:
+        pb_lines_coords_append([pixel_bounds[i],pixel_bounds[0]])
+
+# judgment lines cross which bounds lines.
+which_lines_cross = []
+which_lines_cross_append = which_lines_cross.append
+for j in range(len_cross_lines_coords):
+    for i in range(len_pixel_bounds):
+        is_lines_cross = self.is_intersected_ls(pb_lines_coords[i][0], pb_lines_coords[i][1], cross_lines_coords[j][0], cross_lines_coords[j][1])
+        if is_lines_cross:
+            which_lines_cross_append(i)
+            break
+        elif (not is_lines_cross) and i==len_pixel_bounds-1:
+            which_lines_cross_append(np.nan)
+        
+        
+# Find intersection
+intersection = []
+intersection_append = intersection.append
+for j in range(len_cross_lines_coords):
+    i = which_lines_cross[j]
+    intersec = self.intersection_ls(pb_lines_coords[i][0], pb_lines_coords[i][1], cross_lines_coords[j][0], cross_lines_coords[j][1])
+    intersection_append(intersec)
+    
+intersection = np.array(intersection)
+
+# tile number having cross lines.
+cross_owner = []
+cross_owner_append = cross_owner.append
+for own, is_cross in zip(owner, cross_lines):
+    if is_cross:
+        cross_owner_append(own)
+
+# The coordinates of the intersection of tiles
+tiles_intersection = [[] for i in range(len_tiles)]
+for cross_line_num, cro_own in enumerate(cross_owner):
+    for tile_num in cro_own:
+        tiles_intersection[tile_num].append(intersection[cross_line_num])
+
+# to calculate conveniently, convert numpy.array      
+tiles_intersection = [ [[np.nan,np.nan],[np.nan,np.nan]] if ti==[] else ti for ti in tiles_intersection]
+tiles_intersection = np.array(tiles_intersection)
+
+
+# tile_base_point
+tile_base_points = []
+tile_base_points_append = tile_base_points.append
+for tile_num in range(len_tiles):
+    tile_base_points_append(points[tiles[tile_num][0]])
+tile_base_points = np.array(tile_base_points)
+
+# Convert to coordinates in tile
+for tile_num in range(len_tiles):
+    if np.isnan(tiles_intersection[tile_num]).all():
+        continue
+    tile_base_point = tile_base_points[tile_num]
+    
+    for i in range(len(tile_base_point)):  # 2d:i=0,1
+        tiles_intersection[tile_num][:,i] -= tile_base_point[i]
+
+
+# Calculate which position of which tile is relative to the 4 points of Bounds.
+bounds_corner_tiles_index = []
+bounds_corner_tile_coords = []
+for i in range(len_pixel_bounds):  # i=0,1,2,3 if rectangle
+    corner_jugdment = -(tile_base_points - pixel_bounds[i])
+    tile_corner_index = np.where(np.all((0.0 <= corner_jugdment) * (corner_jugdment[:,[0]] < TILE_SIZE[0]) * (corner_jugdment[:,[1]] < TILE_SIZE[1]), axis=1) )
+    bounds_corner_tiles_index.append(tile_corner_index)
+    bounds_corner_tile_coords.append(corner_jugdment[tile_corner_index])
+    
+bounds_corner_tiles_index = np.array(bounds_corner_tiles_index).reshape(-1,)
+bounds_corner_tile_coords = np.array(bounds_corner_tile_coords).reshape(-1,2)
+
+# Add nan to the coordinates of the third point for the time being
+tiles_intersection = np.concatenate([tiles_intersection, np.full( (len_tiles,1,2) ,np.nan)], axis=1)
+#%%
+bounds_corner_tile_coords
+#%%
+for i in range(len(bounds_corner_tile_coords)):
+    bounds_tile_num = bounds_corner_tiles_index[i]
+    tiles_intersection[bounds_tile_num][2] = bounds_corner_tile_coords[i]
+#%%
+tiles_intersection
+
+#%%
+# check
+#for i in range(len_pixel_bounds):  # i=0,1,2,3 if rectangle
+#    if not self.inpolygon(pixel_bounds[i] , points[tiles[bounds_corner_tiles[i]]]):
+#        raise ValueError
+
+#%%
+
+# tiles have how many points in bounds.
 howmany_points_in_bounds = []
 howmany_points_in_bounds_append = howmany_points_in_bounds.append
-for i in range(len_tile):
-    howmuch_in_bounds = points_is_in_bounds[tile[i]].sum()
+for i in range(len_tiles):
+    howmuch_in_bounds = points_is_in_bounds[tiles[i]].sum()
     howmany_points_in_bounds_append(howmuch_in_bounds)
 
-# pick up tile having at least 1 points in bounds.
+# pick up tiles having at least 1 points in bounds.
 is_in_bounds = [hpib !=0 for hpib in howmany_points_in_bounds]
 
-pickup_tile = points[tile[is_in_bounds,0]] / TILE_SIZE
-pickup_tile = pickup_tile.astype(int)
+# Tile having bounds corner contains, because the case is exists that 
+# tile all points is not in bounds, but bounds corner points is in the tile.
+for index in bounds_corner_tiles_index:
+    is_in_bounds[index] = True
+    
 
-pickup_tile_list = []
-for pit in pickup_tile:
+pickup_tiles = points[tiles[is_in_bounds,0]] / TILE_SIZE
+pickup_tiles = pickup_tiles.astype(int)
+
+pickup_tiles_intersection = tiles_intersection[is_in_bounds]
+
+
+#return pickup_tiles, pickup_tiles_intersection
+
+
+
+
+
+
+
+
+
+
+COMMENTOUT="""
+pickup_tiles_list = []
+for pit in pickup_tiles:
     x = pit[0]
     y = pit[1]
     path = filepath +str(x) + '/'+str(y) + file_extention
-    pickup_tile_list.append(path)
+    pickup_tiles_list.append(path)
 
-if filepath:
-    # all database tile.
-    tilefile_list = glob.glob(filepath+'*/*')
-    isnot_exist_files = set(pickup_tile_list) - set(tilefile_list)
 
-    # if pickup_file is not exist, raise error
-    if not isnot_exist_files==set():
-        raise ValueError(str(isnot_exist_files)+' is not exist')
+# all database tiles.
+tilesfile_list = glob.glob(filepath+'*/*')
+isnot_exist_files = set(pickup_tiles_list) - set(tilesfile_list)
+
+# if pickup_file is not exist, raise error
+if not isnot_exist_files==set():
+    raise ValueError(str(isnot_exist_files)+' is not exist')"""
         
-        
-#%%
-
 
 
 
 #%%
 #path = Path(filepath)
-#tilefile_list = list(path.glob('*/*'))
-#tilefile_list_str = [str(x) for x in tilefile_list]
-#tilefile_list_str
+#tilesfile_list = list(path.glob('*/*'))
+#tilesfile_list_str = [str(x) for x in tilesfile_list]
+#tilesfile_list_str
+
+#%%
+#lines_intersection = np.array([[np.nan, np.nan] for i in range(len_lines)])
+#lines_intersection[cross_lines] = intersection
+
+#%%
+COMMENT_OUT="""
+tiles_owner = [[] for i in range(len_tiles)]  # Which line the tile has
+for i in range(len_lines):
+    len_owner_i = len(owner[i])
+    if len_owner_i==1:
+        tiles_owner[owner[i][0]].append(i)  # [0] is peel list
+    elif len_owner_i==2:
+        tiles_owner[owner[i][0]].append(i)
+        tiles_owner[owner[i][1]].append(i)
+    else:
+        raise KeyError('owner is max 2 length')"""
+        
+        
